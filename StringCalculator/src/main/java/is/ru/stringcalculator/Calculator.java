1ea37de7c3301d4d34edd1ae870df2ea2f3a0aad
Stefán Hafsteinsson <Stefan@Stefans-MacBook-Pro.local>
2014-10-12 18:06:03 +0000

    Refactored test for one and two numbers by adding toInt functoin

--- StringCalculator/src/main/java/is/ru/stringcalculator/Calculator.java
+++ StringCalculator/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -12,14 +12,19 @@ public class Calculator
 		{
 			if(!text.contains(","))
 			{
-				return Integer.parseInt(text);	
+				return toInt(text);	
 			}
 
 			String[] numbers = text.split(",");
 
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			return toInt(numbers[0]) + toInt(numbers[1]);
 			
 		}
 		
+	}
+
+	private static int toInt(String number)
+	{
+		return Integer.parseInt(number);
 	}
 }
